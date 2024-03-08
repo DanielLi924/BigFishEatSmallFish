2537c715b1654f2cb578d9aa438a8e5f9db39248
DanielLi924 <lcd-0507@foxmail.com>
2024-03-09 00:01:43 +0800

    完成了三个按钮的构建，对应的功能正常，但文字的显示效果还需要调整

--- BigFishEatSmallFish.cpp
+++ BigFishEatSmallFish.cpp
@@ -20,9 +20,9 @@ int main()//主函数
 
 int starting()
 {
-	int LoginLeft = 900, LoginTop = 530, LoginRight = 990, LoginDown = 550;
-	int SignUpLeft = 900, SignUpTop = 560, SignUpRight = 990, SignUpDown = 570;
-	int ExitLeft = 900, ExitTop = 580, ExitRight = 990, ExitDown = 600;
+	int LoginLeft = 860, LoginTop = 290, LoginRight = 1060, LoginDown = 390; //Login按钮参数
+	int SignUpLeft = 860, SignUpTop = 490, SignUpRight = 1060, SignUpDown = 590;//SignUp按钮参数
+	int ExitLeft = 860, ExitTop = 690, ExitRight = 1060, ExitDown = 790;//退出按钮参数
 	DrawButten(LoginLeft,LoginTop, LoginRight, LoginDown, "login"); //绘制登录按钮
 	DrawButten(SignUpLeft, SignUpTop, SignUpRight, SignUpDown, "SignUp"); //绘制注册按钮
 	DrawButten(ExitLeft, ExitTop, ExitRight,ExitDown, "Exit"); //绘制退出图标
@@ -65,12 +65,19 @@ int starting()
 				{
 					char Account[20];  // 用于存储输入的用户名
 					char Password[20]; //用于存储输入的密码
-					InputBox(Account,20, "Your Accunt:(In less than 10 character)","Account","Your Account",500,500,false);
-					InputBox(Password, 20, "Your Password:(In less than 10 character)");
+					InputBox(Account,20, "Your Accunt:(In less than 10 character)","Account","Your Account",0,0,false);//获取用户账户
+					InputBox(Password, 20, "Your Password:(In less than 10 character)","Password","Your Password",0,0,false);//获取用户密码
 				}
 				else if (SignUpClicked)
 				{
-
+					char Account[20];
+					char Password[20];
+					InputBox(Account, 20, "Your Accunt:(In less than 10 character)", "Account", "Your Account",0,0,false);//获取用户账户
+					InputBox(Password, 20, "Your Password:(In less than 10 character)", "Password", "Your Password",0,0,false);//获取用户密码
+				}
+				else if (ExitClicked)
+				{
+					exit(0);
 				}
 
 			}
@@ -81,8 +88,8 @@ int starting()
 void DrawButten(int left, int top, int right, int bottom, const char* text) //绘制标准按钮
 {
 	setfillcolor(LIGHTGRAY);
-	solidroundrect(left, top, right, bottom, 50, 50); //样式和大小
-	outtextxy(left + 10, top + 10, text);//输出文字
+	solidroundrect(left, top, right, bottom, 10, 10); //样式和大小
+	settextstyle(left + 10, top + 10, text);//输出文字
 }
 
 bool isPointInsideRectangle(int x, int y, int left, int top, int right, int bottom)
